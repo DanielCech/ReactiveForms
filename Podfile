a778500c501e9c1a52905f5edb787ddeb58f1ad3
Dan Cech <daniel.cech@gmail.com>
2017-05-19 12:05:17 +0200

    Add: initial commit

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+use_frameworks!
+inhibit_all_warnings!
+
+target 'ReactiveForms' do
+  pod 'ReactiveCocoa', '~> 5.0.0'
+end
---
+++ Pods/Target Support Files/Pods-ReactiveForms/Pods-ReactiveForms-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactiveForms : NSObject
+@end
+@implementation PodsDummy_Pods_ReactiveForms
+@end
